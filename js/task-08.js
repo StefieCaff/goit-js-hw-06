a744eaa727bfa9b4d91de5afc5b2d8c3b0c84b5c
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-01-02 14:43:22 -0800

    task 8 not done

--- js/task-08.js
+++ js/task-08.js
@@ -1,16 +1,27 @@
+class User {
+
+    constructor(selector) {
+
+    }
+}
+
+
+
+
 const loginForm = document.querySelector(".login-form");
 
+const email = loginForm.firstElementChild.value;
+// const password = loginForm.password.value;
+console.log(email);
+// console.log(password);
+console.log(loginForm);
+
 loginForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const {
-    elements: { email, Password }
-  } = event.currentTarget;
-    
-    // if (email.value || password.value === " ") {
-    //   Window.alert('All fields must be filled in.Thanks!');  
-    // }
-    console.log(email.value, password.value);
-    
+    event.preventDefault();
+    if (email.value || password.value == " ") {
+        alert('The fields must be filled in. Thanks!');
+        return false;
+    }
 });
 
 
