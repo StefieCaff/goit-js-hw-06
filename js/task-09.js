d915b29feb17f2fb50f9c08cab3fa65551d8ce54
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-01-02 17:10:30 -0800

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
+const changeColorBtn = document.querySelector(".change-color");
+const changedElem = document.querySelector(".color");
+const randomColor = getRandomHexColor();
+
+
+
+console.log(changeColorBtn);
+console.log(changedElem);
+console.log(randomColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColor = () => {
+  changedElem.style.backgroundColor = randomColor;
+};
+
+changeColorBtn.addEventListener = ("click", changeColor);
