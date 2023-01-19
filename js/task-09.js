fdf55b0285437877e39e2c577095a48fc67514d0
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-01-19 13:10:01 -0800

    add comments to task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,8 +1,8 @@
 //grab elements
 const changeColorBtn = document.querySelector(".change-color");
 const changedElem = document.querySelector("body");
-const randomHexColor = document.querySelector(".color");
-const randomHexColorDark = document.querySelector(".color-dark");
+const randomHexColor = document.querySelector(".color");           //in case hex color is very light
+const randomHexColorDark = document.querySelector(".color-dark");  //in case hex color is very dark
 
 // random color arrow function
 // const getRandomHexColor = () => {
@@ -13,9 +13,9 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-changeColorBtn.addEventListener('click', function () {
-  const randomColor = getRandomHexColor();
-  changedElem.style.backgroundColor = randomColor;
-  randomHexColor.textContent = `${randomColor}`;
-  randomHexColorDark.textContent = `${randomColor}`;
+changeColorBtn.addEventListener('click', function () { //something happens when the button is clicked
+  const randomColor = getRandomHexColor();             // constant assigned random color function
+  changedElem.style.backgroundColor = randomColor;     //change the color of the body to random color
+  randomHexColor.textContent = `${randomColor}`;       //show the value of the hex color in black
+  randomHexColorDark.textContent = `${randomColor}`;   //show the value of the hex color in white
 });
