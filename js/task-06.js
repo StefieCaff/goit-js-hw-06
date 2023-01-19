831287b01b3004eabe5f77dc66dc12a8254dfec6
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-01-19 12:29:06 -0800

    update task 6
    again did not understand task fully. needed to be equal not greater than or equal to

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,7 @@
 const symbolInput = document.getElementById('validation-input');
   
 symbolInput.addEventListener('blur', () => {
-    if (symbolInput.value.length >= symbolInput.getAttribute('data-length')) {
+    if (symbolInput.value.length == symbolInput.getAttribute('data-length')) {
         symbolInput.style.borderColor = "#4caf50";
     } else {
         symbolInput.style.borderColor = "#f44336";
